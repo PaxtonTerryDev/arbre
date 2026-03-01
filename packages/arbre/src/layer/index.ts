ab615f8dc4c85683f34c6bfa9660fbfac31d716f
Paxton Terry <paxtonterrydev@gmail.com>
2026-02-28 23:20:04 -0700

    fix: level possibly null

--- packages/arbre/src/layer/index.ts
+++ packages/arbre/src/layer/index.ts
@@ -3,5 +3,5 @@ import { Log } from "../types/log";
 export interface Layer<Payload = unknown, Scope = unknown> {
   handle(
     log: Log<Payload, Scope>,
-  ): Promise<Log<Payload, Scope>> | Log<Payload, Scope>;
+  ): Promise<Log<Payload, Scope> | null> | Log<Payload, Scope> | null;
 }
