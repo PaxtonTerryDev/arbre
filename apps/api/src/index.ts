7b809787c1096dc5c633de52965f0b58eb5eedd7
Paxton Terry <117777648+PaxtonTerryDev@users.noreply.github.com>
2026-03-05 11:49:13 -0700

    refactor: fix formatting

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,17 +1,20 @@
-import { loadConfig } from "./config/index.js"
-import { PostgresStorageAdapter } from "./storage/postgres.js"
-import { createServer } from "./server.js"
+import { loadConfig } from "./config/index.js";
+import { PostgresStorageAdapter } from "./storage/postgres.js";
+import { createServer } from "./server.js";
 
-const config = loadConfig()
+const config = loadConfig();
 
-;(async () => {
-  const adapter = new PostgresStorageAdapter(config.database.url)
-  await adapter.init()
-  const server = createServer(config, adapter)
-  server.listen({ port: config.server.port, host: config.server.host }, (err) => {
-    if (err) {
-      server.log.error(err)
-      process.exit(1)
-    }
-  })
-})()
+(async () => {
+  const adapter = new PostgresStorageAdapter(config.database.url);
+  await adapter.init();
+  const server = createServer(config, adapter);
+  server.listen(
+    { port: config.server.port, host: config.server.host },
+    (err) => {
+      if (err) {
+        server.log.error(err);
+        process.exit(1);
+      }
+    },
+  );
+})();
